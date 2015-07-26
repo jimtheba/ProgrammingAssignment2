c5f2c741a16fca8787382d124a56d75d885749a3
jimtheba <jimtheba@gmail.com>
2015-07-26 13:34:50 -0400

    makeCacheMatrix written
    First function is complete, I need a break!

--- cachematrix.R
+++ cachematrix.R
@@ -6,7 +6,23 @@
 ## This first function creates the matrix object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL      ## Create a variable with a value of null; this will be replaced later
+  ## Create the set function, which sets the value of the matrix with the argument entered in y
+  set <- function(y) {
+    ## Use the <<- operator to set the value of x outside of the current environment
+    x <<- y                 
+    ## Use the <<- operator to set the value of m outside of the current environment
+    m <<- NULL
+  }
+  ## Create the get function, which retrieves the value of x
+  get <- function() x
+  ## Create the setmatrix function, which uses the solve function to return the
+  ## inverse of the matrix; solve returns the inverse if the b argument is missing.
+  setmatrix <- function(solve) m <<- solve
+  ## Create the getmatrix function, which retrieves the value of m
+  getmatrix <- function() m
+  ## Create our list of functions and their corresponding names
+  list(set=set, get=get, setmatrix=setmatrix, getmatrix=getmatrix)
 }
 
 
