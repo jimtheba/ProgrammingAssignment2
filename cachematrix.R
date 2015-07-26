0e31fc1cf73288b556d000cd3f1759d5a2fb4be5
jimtheba <jimtheba@gmail.com>
2015-07-26 13:19:36 -0400

    Comments
    Filled out the comments outside of the functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,16 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The purpose of the two functions makeCacheMatrix and cacheSolve
+## is to created a cached matrix object that holds the inverse of
+## a matrix. This allows for the computation to be done just once
+## instead of computing repeatedly as needed.
 
-## Write a short comment describing this function
+## This first function creates the matrix object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+## This second function computes the inverse of the matrix from makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
